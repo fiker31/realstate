8b11dc21c21fd07b80eb331c271c2d488e6fb572
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-04-11 10:48:51 +0300

    config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-
-module.exports= {
-  reactStrictMode: true,
-  images:{
-    domains:['https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4']
-  }
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
+  },
 }
-
-
