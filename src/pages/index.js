3e88b148880d9e1c030116c5e4e409c24f6b1033
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-04-11 12:02:13 +0300

    initialize fetchapi

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const Banner=({purpose, title1, title2,desc1,desc2,buttonText,linkName,imageUrl}
       <Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700">{desc1}<br/>{desc2}</Text>
       <Button fontSize="xl" bg="blue.300" color="white">
           <Link href={linkName}>{buttonText}</Link>
-
       </Button>
     </Box>
   </Flex>
@@ -20,8 +19,8 @@ const Banner=({purpose, title1, title2,desc1,desc2,buttonText,linkName,imageUrl}
 
 export default function Home() {
   return (
-    <div>
-        <h1>hello</h1>
+    <Box>
+      
         <Banner 
             purpose="RENT A HOME"
             title1="Rental Homes for"
@@ -31,9 +30,14 @@ export default function Home() {
             buttonText="Explore Renting"
             linkName="/search?purpose=for-Rent"
             imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
-            
-            
+                       
             />
+            <Flex flexWrap="wrap">
+            {/*Fetch the properties and map over them*/}
+
+
+
+            </Flex>
         <Banner 
            purpose="BUY A HOME"
            title1="Find, Buy & Own Your"
@@ -43,10 +47,9 @@ export default function Home() {
            buttonText="Explore Renting"
            linkName="/search?purpose=for-sale"
            imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
-           
-            
-            
-            />
-    </div>
+                    
+                        />
+            {/*Fetch the properties and map over them*/}
+    </Box>
   )
 }
