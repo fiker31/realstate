1603748df7cddd4ec99070d26766878e3d76be1b
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-04-11 12:57:03 +0300

    assets

--- components/Property.jsx
+++ components/Property.jsx
@@ -8,7 +8,14 @@ import millify  from "millify";
 
 const Property=({property:{coverPhoto,price,rentFrequency,rooms,title,baths,area,agency,isVerified,externalID}})=>(
     <Link href={`/property/${externalID}`} passHref>
-    {title}
+  <Flex flexWrap="wrap" w="420px"p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
+    <Box>
+        <Image src={coverPhoto ? coverPhoto.url : defaultImage}/>
+    </Box>
+
+
+
+  </Flex>
     </Link>
 );
 export default Property;
